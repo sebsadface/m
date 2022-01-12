3c03bc02cf9722e84c5bed1aa6707a0f8c1826bc
sebsadface <sebsadface@gmail.com>
2022-01-11 21:55:52 -0800

    CP2 update

---
+++ CP2/CP2.py
@@ -0,0 +1,83 @@
+import numpy as np
+
+### Problem 1
+### Initialize A as a 20 by 21 matrix of zeros (Week 2 Lecture 1)
+### To fill in the matrix create a nested for loop (Week 2 Lecture 1)
+### Save the matrix A as the variable A1
+### Don't forget to use .copy()
+
+
+
+
+
+### Let B equal A, and set the entire 15th row as zero (Week 1 Lecture 3)
+### Do the same thing for the entire 16th column
+### Save the matrix B as A2
+### Don't forget to use .copy()
+
+
+
+
+
+### For A3, since we want the last few columns/rows you want to use negative
+### values to go backwards through the circular queue (Week 1 Lecture 3)
+
+
+
+
+
+### Set A4 as the 10th column of B (Week 1 Lecture 3)
+### reshape it into a column vector
+
+
+
+
+
+
+### Problem 2
+### For A5 and A6 it's exactly like Week 2 Theory lecture.
+
+
+
+
+
+### For A7 through A10 you're still doing a Sum as you did for A5 and A6
+### but now you want to break out of the loop when the sum surpasses 10
+### for A7 and A8, and 20 for A9 and A10
+### (very similar to Week 2 Lecture 2 Fibonacci)
+
+
+
+
+
+
+### Problem 3
+### Create a function here (Week 2 Lecture 3).  The function will take r,
+### x0, and N as inputs and output a vector x of all N iterates starting at
+### x0.  Inside the function create a loop that calculates the value of the
+### logistic map and saves it in its respective entries of x.
+
+
+
+
+
+### Set N and x0 according to the assignment file
+
+
+
+
+### For each of the next three set its respective r value.  Then set the
+### vector x as the iterates of the logistic map using the output of the
+### function you created.
+
+### Write the code for A11 and A12 here
+
+
+
+### Do the same as above except for A13 and A14
+
+
+
+### Do the same as above except for A15 and A16
+
+
