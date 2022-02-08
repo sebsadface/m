ec6b26a6fd2a40d9b3cb516b07952bbdcde0e566
sebsadface <sebsadface@gmail.com>
2022-02-07 19:21:47 -0800

    cp5 update

---
+++ CP5/CP5.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+### Problem 1
+### Implement the Bisection method as we did in the Week 5 Coding Lecture
+
+
+
+
+
+
+### Problem 2
+### Implement Newton's method as we did in the Week 5 Coding Lecture
