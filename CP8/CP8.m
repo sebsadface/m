bac334407dd0fd83c8935fbad6c9cbfd1f36b02f
sebsadface <sebsadface@gmail.com>
2022-02-26 11:54:09 -0800

    CP8 update

---
+++ CP8/CP8.m
@@ -0,0 +1,83 @@
+%%% Problem 1
+%%% Initialize t, and x_true
+
+
+
+
+
+%%% Forward Euler
+%%% Write a forward Euler scheme
+
+
+
+
+%%% Backward Euler
+%%% Write a backward Euler scheme
+
+
+
+
+
+%%% Built-in Solver
+%%% Use ode45
+%%% Don't forget to transpose the solution you get from ode45.
+
+
+
+
+
+%%% Problem 2
+%%% Initialize the parameters
+
+
+
+
+%%% Forward Euler for dt = 0.01
+%%% Initialize t and x_true
+%%% Write a forward Euler scheme
+
+
+
+
+
+%%% Forward Euler dt = 0.001
+%%% Reinitialize t and x_true
+%%% Write a forward Euler scheme
+
+
+
+
+
+%%% Predictor-Corrector dt = 0.01
+%%% Reinitialize t and x_true
+%%% Write a forward Euler scheme and a backward Euler scheme in the same
+%%% loop.
+%%% The forward Euler scheme is the predictor.  The answer from forward
+%%% Euler will be plugged into the sin(x_n+1) in the backward Euler scheme.
+
+
+
+
+
+%%% Predictor-Corrector dt = 0.001
+%%% Reinitialize t and x_true
+%%% Write a forward Euler scheme and a backward Euler scheme in the same
+%%% loop.
+%%% The forward Euler scheme is the predictor.  The answer from forward
+%%% Euler will be plugged into the sin(x_n+1) in the backward Euler scheme.
+
+
+
+
+
+%%% Builtin Solver
+%%% Reinitialize t and x_true
+%%% Use ode45 to solve the ODE.
+%%% Don't forget to transpose the solution you get from ode45.
+
+
+
+
+
+
+%%%  If you want to write local functions, put them here
---
+++ CP8/CP8.py
@@ -0,0 +1,75 @@
+import numpy as np
+import scipy.integrate
+######################
+
+### Problem 1
+### Initialize t, and x_true
+
+
+
+### Forward Euler
+### Write a forward Euler scheme
+
+
+
+
+### Backward Euler
+### Write a backward Euler scheme
+
+
+
+
+### Built-in Solver
+### Use scipy.integrate.solve_ivp
+### Don't forget to reshape the solution you get from scipy.integrate.solve_ivp
+
+
+
+
+### Problem 2
+### Initialize the parameters
+
+
+
+
+### Forward Euler for dt = 0.01
+### Initialize t and x_true
+### Write a forward Euler scheme
+
+
+
+
+
+### Forward Euler dt = 0.001
+### Reinitialize t and x_true
+### Write a forward Euler scheme
+
+
+
+
+
+### Predictor-Corrector dt = 0.01
+### Reinitialize t and x_true
+### Write a forward Euler scheme and a backward Euler scheme in the same loop.
+### The forward Euler scheme is the predictor.  The answer from forward
+### Euler will be plugged into the sin(x_n+1) in the backward Euler scheme.
+
+
+
+
+
+### Predictor-Corrector dt = 0.001
+### Reinitialize t and x_true
+### Write a forward Euler scheme and a backward Euler scheme in the same loop.
+### The forward Euler scheme is the predictor.  The answer from forward
+### Euler will be plugged into the sin(x_n+1) in the backward Euler scheme.
+
+
+
+
+### Builtin Solver
+### Reinitialize t and x_true
+### Use scipy.integrate.solve_ivp
+### Don't forget to reshape the solution you get from scipy.integrate.solve_ivp
+
+
