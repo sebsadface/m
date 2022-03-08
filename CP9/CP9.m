92192f7ae19728ba7f0849bd0383c47f26896a45
sebsadface <sebsadface@gmail.com>
2022-03-07 17:04:17 -0800

    CP9 update

---
+++ CP9/CP9.m
@@ -0,0 +1,37 @@
+%%% Problem 1
+%%% Use ode45 to solve the Fitzhugh-Nagumo IVP
+%%% For the maxima use the plot to narrow down the times you use to search
+%%% for the maximum.
+
+
+
+
+%%% Problem 2
+%%% Use ode45 to solve the Chua equation
+%%% You can tell something is chaotic if it is seemingly random
+%%% If it looks like all solutions tend toward a point or a circle it is
+%%% not chaotic.
+
+
+
+%%% Problem 3
+
+%%% Part 1: Finite Differences
+%%% Use finite differences to solve the BVP
+%%% Be careful about the shape of the vectors, you may have to transpose to
+%%% get the correct shape.  It's a good idea to print the solutions out to
+%%% make sure the shape is correct.
+
+
+
+%%% Part 2: Shooting Method via Bisection
+%%% Use the shooting method to solve the BVP
+%%% It's a good idea to test out a few in the command window first to make
+%%% sure that your initial conditions gets you to different sides of the right
+%%% boundary condition.
+%%% Use the plot to help you figure out what your choices of initial
+%%% conditions should be
+
+
+
+%%% You can set up your ODEs as functions here if you like
